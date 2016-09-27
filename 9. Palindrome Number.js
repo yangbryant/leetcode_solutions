2965e6c51aa99b8e7e425d6dfc0a7c1ab4709a74
liyang <liyang@everyoo.com>
2016-09-27 17:44:45 +0800

    add 9 solution file

---
+++ 9. Palindrome Number.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+	let array = x.toString().split('');
+	
+	return true;
+};
+
+var num = 12345654321;
+console.log(isPalindrome(num));
