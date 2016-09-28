3fd90206d338a262d7ab25d76e0062009c34ddfb
liyang <liyang@everyoo.com>
2016-09-28 10:13:53 +0800

    solve 9 question

--- 9. Palindrome Number.js
+++ 9. Palindrome Number.js
@@ -2,11 +2,19 @@
  * @param {number} x
  * @return {boolean}
  */
+//var isPalindrome = function(x) {
+//	let array = x.toString().split('');
+//	for (var i = 0; i < array.length/2; i++) {
+//		if (array[i] !== array[array.length - 1 - i]) {
+//			return false;
+//		}
+//	}
+//	return true;
+//};
+
 var isPalindrome = function(x) {
-	let array = x.toString().split('');
-	
-	return true;
+	return x.toString().split('').reverse().join('') === x.toString() ? true : false;
 };
 
-var num = 12345654321;
+var num = 123456654321;
 console.log(isPalindrome(num));
