4ab6aab95f3868e80485b716241aecee5889a7b5
liyang <liyang@everyoo.com>
2016-10-14 13:52:58 +0800

    add another 58 solution

--- 58. Length of Last Word.js
+++ 58. Length of Last Word.js
@@ -2,15 +2,19 @@
  * @param {string} s
  * @return {number}
  */
+//var lengthOfLastWord = function(s) {
+//	s = s.trim();
+//	var length = s.length;
+//	for (var i = length - 1; i >= 0; i--) {
+//		if (s[i] === " ") {
+//			return length - 1 - i;
+//		}
+//	}
+//	return length;
+//};
+
 var lengthOfLastWord = function(s) {
-	s = s.trim();
-	var length = s.length;
-	for (var i = length - 1; i >= 0; i--) {
-		if (s[i] === " ") {
-			return length - 1 - i;
-		}
-	}
-	return length;
+	return s.trim().length - s.trim().lastIndexOf(" ") - 1;
 };
 
 console.log(lengthOfLastWord(" Hello World! "));
