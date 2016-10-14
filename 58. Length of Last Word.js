34bd37931e4db839cd3364500774e1d5c48619ac
liyang <liyang@everyoo.com>
2016-10-14 13:33:31 +0800

    solve 58 question

---
+++ 58. Length of Last Word.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function(s) {
+	s = s.trim();
+	var length = s.length;
+	for (var i = length - 1; i >= 0; i--) {
+		if (s[i] === " ") {
+			return length - 1 - i;
+		}
+	}
+	return length;
+};
+
+console.log(lengthOfLastWord(" Hello World! "));
